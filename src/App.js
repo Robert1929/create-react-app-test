3e6025004f3fc919e808bfc41a28d9105bb58ed9
Robert Gabriel Ursan <r.ursan@roislpt2298.levi9.com>
2021-07-09 09:37:51 +0300

    Revert "initial commit"
    This reverts commit f33b7ab1e4997ac6cfd71d7694edb06dfe01653a.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import "./App.css";
-import React, { useState } from "react";
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
-function App() {
-  const [number, setNumber] = useState(0);
-  const handleOriginal = () => {
-    console.log("Original");
-    setNumber(number + 1);
-  };
-  const handleVariant = () => {
-    console.log("Variant");
-    setNumber(number - 1);
-  };
-  return (
-    <div>
-      <button style={{ color: "red" }} onClick={handleOriginal}>
-        Add
-      </button>
-      <button style={{ color: "green" }} onClick={handleVariant}>Remove</button>
-      <div>Ai apasat de {number} ori;</div>
-    </div>
-  );
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { loading: false, msg: null }
+  }
+
+  handleClick = api => e => {
+    e.preventDefault()
+
+    this.setState({ loading: true })
+    fetch("/.netlify/functions/" + api)
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }))
+  }
+
+  render() {
+    const { loading, msg } = this.state
+
+    return (
+      <p>
+        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
+        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <br />
+        <span>{msg}</span>
+      </p>
+    )
+  }
 }
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <LambdaDemo />
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
